fd3032cee1e5a34a39a5fe696a80e5d99e46fb4b
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-04-21 20:50:32 +0300

    Add class LinterCheck

--- lib/linter_methods.rb
+++ lib/linter_methods.rb
@@ -1,5 +1,14 @@
-def single_quote(line, num)
-  if line.include?("'")
-    @error_list << "Single quotes on line number #{num}"
+
+
+class LinterCheck
+  def initialize(attribute)
+    @error_list = []
+    @attribute = attribute
+  end
+
+  def single_quote(line, num)
+    if line.include?("'")
+      @error_list << "Single quotes on line number #{num}"
+    end
   end
 end
