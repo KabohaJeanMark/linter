436ca06dc6a9d2b75649ad9581e6340c068e8a5f
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-04-22 12:01:06 +0300

    Add test to check that on initialization an array is stored in the object

--- spec/linter_methods_spec.rb
+++ spec/linter_methods_spec.rb
@@ -1,13 +1,19 @@
 require 'linter_methods'
 
 describe LinterCheck do
+  let(:linter_check_object) {LinterCheck.new('css/style.css')}
   describe '#initialize' do
-    context 'when wrong number of args are passed' do
+    context 'when the object is not created successfully' do
       it 'raises Argument error if no arg is passed' do
         expect {LinterCheck.new}.to raise_error(ArgumentError)
       end
       it 'raises argument error when more than one arg is passed' do
         expect {LinterCheck.new('../css/style', '2ndarg')}.to raise_error(ArgumentError)
+      end
+    end
+    context 'when the object is created successfully' do
+      it 'contains an empty array to store errors' do
+        expect(linter_check_object.error_list).to be_a(Array)
       end
     end
   end
